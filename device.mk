891ca0ff79e467906e488727cc966db4eaea8357
basamaryan <basam.aryan@gmail.com>
2025-01-05 21:52:16 +0530

    redwood: Move to Xiaomi fingerprint AIDL

--- device.mk
+++ device.mk
@@ -198,9 +198,9 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.redwood \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
-    vendor.xiaomi.hardware.fx.tunnel@1.0.vendor
+   vendor.xiaomi.hardware.fx.tunnel@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
